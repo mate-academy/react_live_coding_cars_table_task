283d39de58583587ade2db0e4c25f14495dad551
Andrii Petrinko <apetrinko@gmail.com>
2023-01-13 14:29:58 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-// import carsFromServer from './api/cars';
+import carsFromServer from './api/cars';
 // import colorsFromServer from './api/colors';
 
 // 1. Render car with color
 // 2. Add ability to filter car by brand name
 // 3. Add ability to filter car by color
+
+const findCarbyId = (colorId: number) => {
+  carsFromServer.find(el => el.id === colorId);
+};
+
+const carWithColor = carsFromServer.map(el => ({
+  ...el,
+  color: findCarbyId(el.colorId),
+}));
 
 export const App: React.FC = () => {
   return (
@@ -18,31 +27,21 @@ export const App: React.FC = () => {
       <table>
         <thead>
           <tr>
-            <th>Id</th>
-            <th>Brand</th>
-            <th>Color</th>
-            <th>Rent price</th>
+            <th>id</th>
+            <th>name</th>
+            <th>color</th>
+            <th>price</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Ferarri</td>
-            <td style={{ color: 'red' }}>Red</td>
-            <td>500</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Opel</td>
-            <td style={{ color: 'white' }}>White</td>
-            <td>300</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Audi</td>
-            <td style={{ color: 'black' }}>Black</td>
-            <td>300</td>
-          </tr>
+          {carWithColor.map(el => (
+            <tr>
+              <td>{el.id}</td>
+              <td>{el.brand}</td>
+              <td style={{ color: 'red' }}>color</td>
+              <td>{el.rentPrice}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
