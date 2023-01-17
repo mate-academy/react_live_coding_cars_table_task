5f2d99d402860aad69e7f034cd80cd76dcb346d3
kreisberg <anastasia.kreisberg@yahoo.com>
2023-01-17 13:33:32 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
-// import carsFromServer from './api/cars';
-// import colorsFromServer from './api/colors';
+import React, { useState } from 'react';
+import colors from './api/colors';
+import carsFromServer from './api/cars';
+import { FullCarsList } from './types';
 
 // 1. Render car with color
 // 2. Add ability to filter car by brand name
 // 3. Add ability to filter car by color
 
+const finColor = (colorId: number) => (
+  colors.find(c => c.id === colorId)
+);
+
+const getNewColoredCar = (): FullCarsList[] => (
+  carsFromServer.map(car => ({
+    ...car,
+    color: finColor(car.colorId),
+  }))
+);
+
 export const App: React.FC = () => {
+  const [cars] = useState(getNewColoredCar);
+  const [query, setQuery] = useState('');
+
+  // const getNewCars = () => (
+  //   cars.map(car => car.brand.toLowerCase().includes(query.toLowerCase()))
+  // );
+
+  // const visibleCars = getNewCars();
+
+  // if (visibleCars) {
+  //   cars.filter(car => car.brand.toLowerCase().includes(query.toLowerCase()));
+  // }
+
   return (
     <div>
-      <input type="search" placeholder="Find by car brand" />
+      <input
+        type="search"
+        placeholder="Find by car brand"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
 
       <select>
         <option>Chose a color</option>
@@ -17,33 +47,15 @@ export const App: React.FC = () => {
 
       <table>
         <thead>
-          <tr>
-            <th>Id</th>
-            <th>Brand</th>
-            <th>Color</th>
-            <th>Rent price</th>
-          </tr>
+          {cars.map(car => (
+            <tr>
+              <th>{car.id}</th>
+              <th>{car.brand}</th>
+              <th>{car.color?.name}</th>
+              <th>{car.rentPrice}</th>
+            </tr>
+          ))}
         </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Ferarri</td>
-            <td style={{ color: 'red' }}>Red</td>
-            <td>500</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Opel</td>
-            <td style={{ color: 'white' }}>White</td>
-            <td>300</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Audi</td>
-            <td style={{ color: 'black' }}>Black</td>
-            <td>300</td>
-          </tr>
-        </tbody>
       </table>
     </div>
   );
