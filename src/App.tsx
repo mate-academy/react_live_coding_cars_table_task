03315893200cb00594c1886a51a5634e7d0119ea
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-01-17 14:17:56 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
-// import carsFromServer from './api/cars';
-// import colorsFromServer from './api/colors';
+import { useState } from 'react';
+import carsFromServer from './api/cars';
+import colorsFromServer from './api/colors';
+import { CarFull } from './types/Types';
 
 // 1. Render car with color
 // 2. Add ability to filter car by brand name
 // 3. Add ability to filter car by color
+const fullCar:CarFull[] = carsFromServer.map(car => {
+  const color = colorsFromServer.find(colorCar => colorCar.id === car.colorId);
+
+  return {
+    ...car,
+    color,
+  };
+});
 
 export const App: React.FC = () => {
+  const [carName, setCarName] = useState('');
+  const [cars] = useState(fullCar);
+  let filteredCarsByName = cars;
+
+  if (carName) {
+    filteredCarsByName = cars
+      .filter(car => (car.brand.toLowerCase()).includes(carName.toLowerCase()));
+  }
+
   return (
     <div>
-      <input type="search" placeholder="Find by car brand" />
+      <input
+        type="search"
+        placeholder="Find by car brand"
+        value={carName}
+        onChange={(event) => setCarName(event.target.value)}
+      />
 
-      <select>
+      <select value={0}>
         <option>Chose a color</option>
       </select>
 
@@ -25,24 +49,14 @@ export const App: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Ferarri</td>
-            <td style={{ color: 'red' }}>Red</td>
-            <td>500</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Opel</td>
-            <td style={{ color: 'white' }}>White</td>
-            <td>300</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Audi</td>
-            <td style={{ color: 'black' }}>Black</td>
-            <td>300</td>
-          </tr>
+          {filteredCarsByName.map(car => (
+            <tr>
+              <td>{car.id}</td>
+              <td>{car.brand}</td>
+              <td style={{ color: car.color?.name }}>{car.color?.name}</td>
+              <td>{car.rentPrice}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
